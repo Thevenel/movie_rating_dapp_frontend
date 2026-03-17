2499870d5f1a916805d5c3582ab265535251dfb9
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-03-17 14:46:36 +0100

    fixed ratting average

--- src/app/services/movie-rating.ts
+++ src/app/services/movie-rating.ts
@@ -10,7 +10,6 @@ export type Movie = {
   totalRating: number;
   ratingCount: number;
   canRate? : boolean;
-  userRating? : number;
 };
 
 @Injectable({
@@ -60,28 +59,6 @@ async init() {
 
 }
 
-
-//   async init() {
-//   if ((window as any).ethereum) {
-//     this.provider = new ethers.BrowserProvider((window as any).ethereum);
-//     await this.provider.send("eth_requestAccounts", []); // prompt MetaMask
-//     const signer = await this.provider.getSigner();
-//     const contractAddress = environment.contractAddress; // <-- UPDATE THIS
-//     this.contract = new ethers.Contract(contractAddress, MovieRatingJson.abi, signer);
-//   } else {
-//     alert('Please install MetaMask!');
-//   }
-//   const network = (await this.provider.getNetwork());
-//   console.log("Network Name:", network.name);
-//   console.log("Network Chain ID:", network.chainId);
-//   console.log("Network Plugins:", network.plugins);
-//   console.log("Contract:", this.contract.target);
-
-//   const code = await this.provider.getCode(this.contract.target);
-//   console.log("Code at address:", code);
-// }
-
-
   private async waitForInit() {
     while (!this.contract) {
       await new Promise((resolve) => setTimeout(resolve, 50));
@@ -94,52 +71,17 @@ async init() {
     await tx.wait();
   }
 
-  // async rateMovie(index: number, rating: number) {
-  //   await this.waitForInit();
-  //   const tx = await this.contract['rateMovie'](index, rating);
-  //   await tx.wait();
-  // }
-
   async rateMovie(index: number, rating: number) {
   await this.waitForInit();
-  
-  
-  // DEBUG LOGS
-  console.log("--- Attempting to Rate Movie ---");
-  console.log("Movie Index (ID):", index);
-  console.log("Rating Value being sent:", rating);
-  console.log("Type of Rating:", typeof rating);
-
 
   try {
     const tx = await this.contract['rateMovie'](index, rating);
-    console.log("Transaction sent! Hash:", tx.hash);
-    
     await tx.wait();
-    console.log("Transaction Confirmed!");
+
   } catch (error) {
     console.error("Transaction Failed:", error);
   }
 }
-
-
-// async getMovies(): Promise<Movie[]> {
-//   await this.waitForInit();
-//   const raw: any[] = await this.contract['getMovies']();
-//   const userAddress = await (await this.provider.getSigner()).getAddress();
-//   console.log("Raw movies:", raw);
-//   return Promise.all(raw.map(async (m, index) =>{
-//     const alreadyRated = await this.contract['hasRated'](index, userAddress);
-//     return {
-//     id: Number(m.id ?? index),
-//     title: m.title,
-//     year: Number(m.year),
-//     totalRating: Number(m.totalRating),
-//     ratingCount: Number(m.ratingCount),
-//     canRate : !alreadyRated
-//   };
-// }));
-// }
 
 async getMovies(): Promise<Movie[]> {
   await this.waitForInit();
@@ -167,8 +109,7 @@ async getMovies(): Promise<Movie[]> {
       year: Number(m.year),
       totalRating: Number(m.totalRating),
       ratingCount: Number(m.ratingCount),
-      canRate: !alreadyRated,
-      userRating: 1
+      canRate: !alreadyRated
     };
   }));
 }
